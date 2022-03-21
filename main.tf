825fda44c7c0b00bf31f633c48fa93375e0b3cf2
Kyle Manoleff <kmanoleff@il2000.com>
2022-03-21 03:00:29 -0700

    database and secrets manager

--- main.tf
+++ main.tf
@@ -168,5 +168,41 @@ resource "aws_lambda_permission" "api_gw" {
   source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
 }
 
+resource "random_password" "master_password" {
+  length  = 16
+  special = false
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  db_name              = "dentist"
+  username             = "admin"
+  password             = random_password.master_password.result
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+  publicly_accessible  = true
+}
+
+resource "aws_secretsmanager_secret" "rds_credentials" {
+  name = "dentist_credentials"
+}
+
+resource "aws_secretsmanager_secret_version" "rds_credentials" {
+  secret_id     = aws_secretsmanager_secret.rds_credentials.id
+  secret_string = <<EOF
+{
+  "username": "${aws_db_instance.default.username}",
+  "password": "${random_password.master_password.result}",
+  "engine": "mysql",
+  "host": "${aws_db_instance.default.endpoint}",
+  "port": ${aws_db_instance.default.port},
+  "database": "${aws_db_instance.default.db_name}"
+}
+EOF
+}
+
 
 
